dd3199ed822e684c1a76e1a718196a160d3e1272
JuyeonBae <99193199+juyeon-Bae@users.noreply.github.com>
2023-04-04 09:26:07 +0900

    group name 출력하기

---
+++ Test_03.java
@@ -0,0 +1,16 @@
+public class Test_03
+{
+
+	public static void main(String args[])
+{
+
+
+	String group="방탄소년단";
+	String name ="뷔(김태형)";	
+	 
+	System.out.println(group+ " "+name);	
+
+	}
+	
+	
+}
