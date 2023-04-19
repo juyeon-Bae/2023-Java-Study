4cf9c814f9ebaceffee46704cce0c4d0950946e6
JuyeonBae <99193199+juyeon-Bae@users.noreply.github.com>
2023-04-19 09:24:10 +0900

    관계 연산자

---
+++ Book_03.java
@@ -0,0 +1,15 @@
+class Book_03
+{
+	public static void main(String[] args) 
+	{
+		int a =20, b= 10;
+	
+		System.out.println("a > b ? " +(a>b)); //관계 연산자
+		System.out.println("a >= b ? " +(a>=b));
+		System.out.println("a < b ? " +(a<b));
+		System.out.println("a <= b ? " +(a<=b));
+		System.out.println("a != b ? " +(a!=b));
+
+
+	}
+}
