aba4962821c3bc336fb54eee2c2fa64d88950654
JuyeonBae <99193199+juyeon-Bae@users.noreply.github.com>
2023-04-25 10:21:54 +0900

    자동형변환, 강제형변환 복습
    04.25 JAVA

---
+++ DataTypeExam1.java
@@ -0,0 +1,14 @@
+class DataTypeExam1 
+{
+	public static void main(String[] args) 
+	{
+		/*int a= 2147483647;
+		int b= 10;
+		int c=a+b;
+		System.out.println(c); //논리형 에러*/
+//		char c ='A'; //저장값은 아스키코드 값
+
+	
+
+	}
+}
